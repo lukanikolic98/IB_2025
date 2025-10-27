484c19613bae8d99514d17c50384ceac357a7546
Luka Nikolic <lukanikolic98@hotmail.com>
2025-10-27 21:32:21 +0100

    Added missing config

--- backend/pki/src/main/java/com/ftn/pki/repository/UserRepository.java
+++ backend/pki/src/main/java/com/ftn/pki/repository/UserRepository.java
@@ -11,6 +11,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
 
   Optional<User> findById(Long id);
-
-  User findByActivationToken(String token);
 }
